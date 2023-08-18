5ec7e109b31b55ce57c1d7702a96307018ce6744
yuta <gssbtsm999@gmail.com>
2023-08-18 12:22:44 +0900

    wip

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -99,7 +99,12 @@ export default function ContactForm() {
 
   return (
     <Container>
-      <form onSubmit={handleSubmit} name={FORM_NAME} data-netlify="true">
+      <form
+        onSubmit={handleSubmit}
+        name={FORM_NAME}
+        data-netlify="true"
+        netlify-honeypot="bot-field"
+      >
         <input type="hidden" name="form-name" value="contact" />
         <div className="space-y-3">
           <div>
