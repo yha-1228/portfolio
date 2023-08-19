0c26c5880a1ecf5bd9585d27a433882ee7d571ba
yuta <gssbtsm999@gmail.com>
2023-08-19 16:52:05 +0900

    refactor: fix type

--- src/lib/netlify/encode.ts
+++ src/lib/netlify/encode.ts
@@ -1,7 +1,9 @@
 /**
  * @see https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
  */
-const encode = (data: { [k: string]: string }) => {
+const encode = (data: {
+  [k: string]: Parameters<typeof encodeURIComponent>[0];
+}) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
