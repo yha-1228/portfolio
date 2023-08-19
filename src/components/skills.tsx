a64a8e0023e6315baa28a0d3d3a06f05d021c5d3
yuta <gssbtsm999@gmail.com>
2023-08-20 05:08:22 +0900

    feat: ダミーコンテンツ更新

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,5 +1,58 @@
 import Container from './ui/container';
 
 export default function Skills() {
-  return <Container>ここにスキルが入ります</Container>;
+  return (
+    <Container className="bg-slate-100">
+      <p>
+        Minim dolore adipisicing ut voluptate ea. Anim esse ullamco voluptate
+        voluptate aute ea laboris occaecat culpa pariatur nulla. Cupidatat ut
+        proident esse aliquip aliqua minim incididunt reprehenderit adipisicing
+        aliquip eiusmod ullamco quis nisi in. Ad quis incididunt officia
+        proident aute laborum commodo qui enim laboris quis pariatur velit
+        fugiat. Qui consectetur pariatur cupidatat in pariatur nisi officia
+        fugiat in enim nisi fugiat cupidatat excepteur. Incididunt sit proident
+        aute labore sunt non in esse tempor quis enim.
+      </p>
+      <p>
+        Minim dolore adipisicing ut voluptate ea. Anim esse ullamco voluptate
+        voluptate aute ea laboris occaecat culpa pariatur nulla. Cupidatat ut
+        proident esse aliquip aliqua minim incididunt reprehenderit adipisicing
+        aliquip eiusmod ullamco quis nisi in. Ad quis incididunt officia
+        proident aute laborum commodo qui enim laboris quis pariatur velit
+        fugiat. Qui consectetur pariatur cupidatat in pariatur nisi officia
+        fugiat in enim nisi fugiat cupidatat excepteur. Incididunt sit proident
+        aute labore sunt non in esse tempor quis enim.
+      </p>
+      <p>
+        Minim dolore adipisicing ut voluptate ea. Anim esse ullamco voluptate
+        voluptate aute ea laboris occaecat culpa pariatur nulla. Cupidatat ut
+        proident esse aliquip aliqua minim incididunt reprehenderit adipisicing
+        aliquip eiusmod ullamco quis nisi in. Ad quis incididunt officia
+        proident aute laborum commodo qui enim laboris quis pariatur velit
+        fugiat. Qui consectetur pariatur cupidatat in pariatur nisi officia
+        fugiat in enim nisi fugiat cupidatat excepteur. Incididunt sit proident
+        aute labore sunt non in esse tempor quis enim.
+      </p>
+      <p>
+        Minim dolore adipisicing ut voluptate ea. Anim esse ullamco voluptate
+        voluptate aute ea laboris occaecat culpa pariatur nulla. Cupidatat ut
+        proident esse aliquip aliqua minim incididunt reprehenderit adipisicing
+        aliquip eiusmod ullamco quis nisi in. Ad quis incididunt officia
+        proident aute laborum commodo qui enim laboris quis pariatur velit
+        fugiat. Qui consectetur pariatur cupidatat in pariatur nisi officia
+        fugiat in enim nisi fugiat cupidatat excepteur. Incididunt sit proident
+        aute labore sunt non in esse tempor quis enim.
+      </p>
+      <p>
+        Minim dolore adipisicing ut voluptate ea. Anim esse ullamco voluptate
+        voluptate aute ea laboris occaecat culpa pariatur nulla. Cupidatat ut
+        proident esse aliquip aliqua minim incididunt reprehenderit adipisicing
+        aliquip eiusmod ullamco quis nisi in. Ad quis incididunt officia
+        proident aute laborum commodo qui enim laboris quis pariatur velit
+        fugiat. Qui consectetur pariatur cupidatat in pariatur nisi officia
+        fugiat in enim nisi fugiat cupidatat excepteur. Incididunt sit proident
+        aute labore sunt non in esse tempor quis enim.
+      </p>
+    </Container>
+  );
 }
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,11 +1,19 @@
 import Layout from '@/components/layout';
 import Container from '@/components/ui/container';
 
-export default function Experience() {
+export default function Blog() {
   return (
-    <Layout title="職務経歴">
+    <Layout title="ブログ">
       <Container>
-        <br />
+        <p>
+          Duis consequat elit non. Non sunt cupidatat incididunt consectetur
+          quis veniam anim id minim veniam consectetur consectetur. Exercitation
+          sunt officia est occaecat mollit et culpa nisi Lorem adipisicing minim
+          tempor et commodo. Amet duis est quis pariatur enim incididunt do
+          aliqua tempor excepteur. Exercitation ea est excepteur officia
+          incididunt ut non occaecat. Sunt incididunt excepteur culpa commodo
+          reprehenderit cupidatat cillum mollit consequat sint anim anim.
+        </p>
         <br />
         <p>
           Duis consequat elit non. Non sunt cupidatat incididunt consectetur
