99156cc96c95dffdafa9b3aaad7e2afcebf144c5
yuta <gssbtsm999@gmail.com>
2023-08-21 15:30:44 +0900

    feat: スキル一覧 UI (#3)

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FiExternalLink } from 'react-icons/fi';
+import { twMerge } from 'tailwind-merge';
+import Container from '../ui/container';
+
+const footerItems = [
+  {
+    href: 'https://github.com/yha-1228',
+    label: 'GitHub',
+  },
+  {
+    href: 'https://zenn.dev/yhase_rqp',
+    label: 'Zenn',
+  },
+  {
+    href: 'https://codepen.io/yh10050846',
+    label: 'Codepen',
+  },
+];
+
+export default function Footer() {
+  return (
+    <footer>
+      <Container>
+        <div
+          className={twMerge(
+            'space-y-2 border-t border-solid border-t-gray-200 py-8',
+            'md:flex md:items-center md:justify-between md:space-y-0'
+          )}
+        >
+          <ul className="flex justify-center space-x-4">
+            {footerItems.map((item) => (
+              <li key={item.href}>
+                <a
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center space-x-1 text-xl font-bold hover:underline"
+                >
+                  <span>{item.label}</span>
+                  <FiExternalLink />
+                </a>
+              </li>
+            ))}
+          </ul>
+          <div className="text-center">
+            <p className="text-sm text-gray-500">
+              Yuta Hasegawa &copy; {new Date().getFullYear()}
+            </p>
+          </div>
+        </div>
+      </Container>
+    </footer>
+  );
+}
---
+++ src/components/ui/heading2.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { twMerge } from 'tailwind-merge';
+
+type Heading2Props = React.ComponentPropsWithRef<'h2'>;
+
+const Heading2 = React.forwardRef<HTMLHeadingElement, Heading2Props>(
+  (props, ref) => {
+    const { className, ...restProps } = props;
+
+    return (
+      <h3
+        className={twMerge(
+          'text-xl font-bold text-blue-500 lg:text-2xl',
+          className
+        )}
+        {...restProps}
+        ref={ref}
+      />
+    );
+  }
+);
+
+Heading2.displayName = 'Heading2';
+
+export default Heading2;
