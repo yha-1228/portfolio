29d65c2ccf2382e3ca879b2c1a0541cae89cf443
yuta <gssbtsm999@gmail.com>
2023-08-22 15:54:03 +0900

    refactor: error boundaryを追加 (#50)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,25 +1,15 @@
+import ErrorDisplay from '@/components/error-display';
 import Layout from '@/components/layout';
 import { ButtonLink } from '@/components/ui/button';
-import Container from '@/components/ui/container';
 
-export default function NotFound() {
+export default function Error404() {
   return (
     <Layout title="ページが見つかりません">
-      <div className="pb-14 pt-8">
-        <Container>
-          <div className="space-y-10 text-center">
-            <section className="space-y-3">
-              <h1 className="text-2xl font-bold">ページが見つかりません</h1>
-              <div className="leading-[1.5]">
-                アクセスしたページは存在しないか、削除された可能性があります。
-              </div>
-            </section>
-            <div>
-              <ButtonLink href="/">ホームに戻る</ButtonLink>
-            </div>
-          </div>
-        </Container>
-      </div>
+      <ErrorDisplay
+        heading="ページが見つかりません"
+        detail="アクセスしたページは存在しないか、削除された可能性があります。"
+        action={<ButtonLink href="/">ホームに戻る</ButtonLink>}
+      />
     </Layout>
   );
 }
