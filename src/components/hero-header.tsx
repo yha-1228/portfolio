b6a39457bd0697db868d8e4b8809f196db58805c
yuta <gssbtsm999@gmail.com>
2023-08-23 07:01:06 +0900

    feat(hero-header): 色調とレイアウト更新

--- src/components/hero-header.tsx
+++ src/components/hero-header.tsx
@@ -3,21 +3,18 @@ import Container from './ui/container';
 
 export default function HeroHeader() {
   return (
-    <div className="py-9">
+    <div className="bg-gray-100 py-9">
       <Container>
-        <p className="text-2xl font-bold leading-[1.35]">
+        <p className="text-2xl font-bold leading-[1.35] lg:text-3xl">
           ユーザーフレンドリーで堅牢なWebを開発します。
         </p>
-        <div className="mt-7 space-y-2">
-          <p className="leading-[1.5]">
-            新卒でシステム開発企業に入社し、主にWebアプリ開発のフロントエンドを担当してきました。
-          </p>
-          <p className="leading-[1.5]">
-            マークアップ技術とReactを利用し、ストレス無く快適に閲覧や操作を行える体験を提供できるよう心掛けています。
+        <div className="mt-8 space-y-2">
+          <p className="lg:text-lg">
+            新卒でシステム開発企業に入社し、主にWebアプリ開発のフロントエンドを担当してきました。マークアップ技術とReactを利用して、ストレス無く快適に閲覧や操作を行える体験を提供できるよう心掛けています。
           </p>
         </div>
 
-        <div className="mt-10">
+        <div className="mt-11">
           <ButtonLink
             href="/experience"
             fullWidth
@@ -27,7 +24,7 @@ export default function HeroHeader() {
           </ButtonLink>
           <ButtonLink
             href="/experience"
-            className="hidden font-bold lg:inline-block"
+            className="hidden w-52 font-bold lg:inline-block"
           >
             職務経歴を見る
           </ButtonLink>
