6851f80e7e6815615d1c5c5fbc4c172aca81d4e2
yuta <gssbtsm999@gmail.com>
2023-08-31 02:30:59 +0900

    fix: fix types

--- src/lib/microcms/types.ts
+++ src/lib/microcms/types.ts
@@ -5,11 +5,11 @@ export type BlogContent = {
   title: string;
   body: string;
   tag: {
-    id: '6xcvtv-9a1';
-    createdAt: '2023-08-29T21:40:54.541Z';
-    updatedAt: '2023-08-29T21:40:54.541Z';
-    publishedAt: '2023-08-29T21:40:54.541Z';
-    revisedAt: '2023-08-29T21:40:54.541Z';
+    id: string;
+    createdAt: string;
+    updatedAt: string;
+    publishedAt: string;
+    revisedAt: string;
     tagName: TagName;
   };
   createdAt: string;
