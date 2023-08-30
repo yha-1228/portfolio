7e6558ab491eef71c9d24483013f9cba57e1122c
yuta <gssbtsm999@gmail.com>
2023-08-31 02:56:57 +0900

    refactor(routes): 不要項目を削除

--- src/routes.ts
+++ src/routes.ts
@@ -2,21 +2,17 @@ export const routes = {
   index: {
     href: '/',
     label: 'トップ',
-    hierarchy: 1,
   },
   experience: {
     href: '/experience',
     label: '職務経歴',
-    hierarchy: 1,
   },
   blog: {
     href: '/blog',
     label: 'ブログ',
-    hierarchy: 1,
     routes: {
       ':id': {
         generateHref: (id: string) => `/blog/${id}`,
-        hierarchy: 2,
       },
     },
   },
