73602d226fb677c202cf6f88e49490574630d88a
yuta <gssbtsm999@gmail.com>
2023-08-31 17:01:28 +0900

    refactor: エラー画面を整理

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,20 +1,8 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import ErrorDisplay from '@/components/ui/error-display';
-import useTitle from '@/hooks/use-title';
+import { ErrorBoard } from '@/components/ui/error-display';
 import { NextErrorProps } from '@/lib/next/types';
-import { SITE_TITLE } from '../../constants';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Error({ error, reset }: NextErrorProps) {
-  useTitle(`${SITE_TITLE} | エラーが発生しました`);
-
-  return (
-    <ErrorDisplay
-      heading="エラーが発生しました"
-      detail="予期せぬエラーが発生しました。"
-      action={<Button onClick={reset}>問題の箇所をもう一度読み込む</Button>}
-    />
-  );
+  return <ErrorBoard error={error} reset={reset} />;
 }
