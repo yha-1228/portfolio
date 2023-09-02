a60f38612d169a24cb0043c6b3b66894329e491e
yuta <gssbtsm999@gmail.com>
2023-09-02 09:52:50 +0900

    refactor: className結合コードをシンプルにする

--- src/components/ui/styled/button.tsx
+++ src/components/ui/styled/button.tsx
@@ -11,12 +11,14 @@ type ButtonBaseProps = {
   rightIcon?: React.ReactNode;
 };
 
-const createClassName = (className?: string) => {
-  return clsx(
-    'inline-flex items-center justify-center rounded-lg bg-primary-600 px-5 py-2 font-bold text-white transition-colors duration-200 ease-out hover:bg-primary-700 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary-300 active:bg-primary-700',
-    className,
-  );
-};
+const baseClassName = clsx(
+  'inline-flex items-center justify-center rounded-lg px-5 py-2 font-bold',
+  'transition-colors duration-200 ease-out',
+  'bg-primary-600 text-white',
+  'hover:bg-primary-700',
+  'active:bg-primary-700',
+  'focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-primary-300',
+);
 
 // ----------------------------------------
 
@@ -28,11 +30,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 
     return (
       <button
-        className={createClassName(
-          clsx(
-            'disabled:cursor-not-allowed disabled:bg-gray-disabled',
-            className,
-          ),
+        className={clsx(
+          baseClassName,
+          'disabled:cursor-not-allowed disabled:bg-gray-disabled',
+          className,
         )}
         {...restProps}
         ref={ref}
@@ -62,8 +63,10 @@ const ButtonLink = React.forwardRef<HTMLAnchorElement, ButtonLinkProps>(
 
     return (
       <Link
-        className={createClassName(
-          clsx(disabled && 'pointer-events-none bg-gray-disabled', className),
+        className={clsx(
+          baseClassName,
+          disabled && 'pointer-events-none bg-gray-disabled',
+          className,
         )}
         aria-disabled={disabled}
         {...restProps}
