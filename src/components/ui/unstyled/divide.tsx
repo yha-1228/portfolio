84713bd33323ff96dea30f40825930f63b9c323c
yuta <gssbtsm999@gmail.com>
2023-09-03 10:59:16 +0900

    refactor(skills)

--- src/components/ui/unstyled/divide.tsx
+++ src/components/ui/unstyled/divide.tsx
@@ -1,13 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 
 type DivideProps = {
   children: React.ReactNode[];
   by: React.ReactNode;
+  /**
+   * @default "div"
+   */
+  as?: React.ElementType<any>;
+  className?: string;
+  style?: React.CSSProperties;
 };
 
-export default function Divide({ children, by }: DivideProps) {
+export default function Divide(props: DivideProps) {
+  const { children, by, as: Wrapper = 'div', className, style } = props;
+
   return (
-    <>
+    <Wrapper className={className} style={style}>
       {React.Children.map(children, (child, index) => {
         if (index === 0) return child;
         return (
@@ -17,6 +26,6 @@ export default function Divide({ children, by }: DivideProps) {
           </>
         );
       })}
-    </>
+    </Wrapper>
   );
 }
