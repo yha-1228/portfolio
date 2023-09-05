deca38ae70cfa95c49deb7dc24ae6a0c4fb7dff5
yuta <gssbtsm999@gmail.com>
2023-09-06 04:16:23 +0900

    docs(map-object): add examples

--- src/utils/object/map-object.ts
+++ src/utils/object/map-object.ts
@@ -1,3 +1,23 @@
+/**
+ * @example
+ * ```ts
+ * const user = { id: 1, name: 'John', age: 18 };
+ *
+ * mapObject(user, (value, key) => ({ field: key, value }))
+ * // output:
+ * // {
+ * //   id: { field: 'id', value: 1 },
+ * //   name: { field: 'name', value: 'John' },
+ * //   age: { field: 'age', value: 18 }
+ * // }
+ *
+ * mapObject(user, null);
+ * // output:
+ * // { id: null, name: null, age: null }
+ *
+ *
+ * ```
+ */
 export default function mapObject<T extends object, U>(
   object: T,
   condition: ((value: T[keyof T], key: keyof T) => U) | U,
