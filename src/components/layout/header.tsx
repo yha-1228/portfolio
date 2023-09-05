6ae3aa2bc26f478168e1199872f23dcb3af8544c
yuta <gssbtsm999@gmail.com>
2023-09-06 07:20:48 +0900

    feat(header): heightを一定にする

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,6 +6,7 @@ import { BsList, BsX } from 'react-icons/bs';
 import ActiveLink from '@/lib/next/components/active-link';
 import { routes } from '@/routes';
 import clsx from '@/utils/css/clsx';
+import createStyleAttr from '@/utils/react/create-style-attr';
 import Container from '../ui/styled/container';
 
 const routesWithoutHome = Object.values(routes).filter(
@@ -16,9 +17,14 @@ export default function Header() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="border-b border-solid border-b-gray-light-300">
+    <header
+      className="border-b border-solid border-b-gray-light-300"
+      style={createStyleAttr({
+        '--root-height': '4rem',
+      })}
+    >
       <Container as="nav">
-        <div className="relative flex h-[4rem] items-center justify-between sm:h-[4.5rem]">
+        <div className="relative flex h-[var(--root-height)] items-center justify-between">
           <Link
             href="/"
             className="text-2xl font-bold transition-colors duration-200 ease-out hover:text-gray-foreground-weak"
@@ -49,15 +55,15 @@ export default function Header() {
                 <ActiveLink
                   href={route.href}
                   className={clsx(
-                    'relative inline-flex items-center px-3 sm:h-[4.5rem]',
-                    'font-bold text-gray-foreground/50',
+                    'relative inline-flex h-[var(--root-height)] items-center px-3',
+                    'font-bold text-gray-foreground/70',
                     'transition-colors duration-200 ease-out',
                     "hover:text-primary-600 hover:before:absolute hover:before:bottom-0 hover:before:left-0 hover:before:h-0.5 hover:before:w-full hover:before:bg-transparent hover:before:content-['']",
                     'active:bg-gray-light-100',
                     'focus-visible:outline focus-visible:outline-2 focus-visible:outline-primary-300',
 
                     'data-[active]:font-bold data-[active]:text-primary-600',
-                    "data-[active]:before:absolute data-[active]:before:bottom-0 data-[active]:before:left-0 data-[active]:before:h-0.5 data-[active]:before:w-full data-[active]:before:bg-primary-600 data-[active]:before:content-['']",
+                    "data-[active]:before:absolute data-[active]:before:bottom-0 data-[active]:before:left-0 data-[active]:before:h-[3px] data-[active]:before:w-full data-[active]:before:bg-primary-600 data-[active]:before:content-['']",
                     'data-[active]:hover:text-primary-600 data-[active]:hover:before:bg-primary-600',
                   )}
                 >
