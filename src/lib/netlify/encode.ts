75dd0c13c0f6f443a7e88738ae1a86005b2de4bb
yuta <gssbtsm999@gmail.com>
2024-06-05 02:55:38 +0900

    refactor(types): add Dict type

--- src/lib/netlify/encode.ts
+++ src/lib/netlify/encode.ts
@@ -1,9 +1,9 @@
+import type { Dict } from '@/types/utils';
+
 /**
  * @see https://www.netlify.com/blog/2017/07/20/how-to-integrate-netlifys-form-handling-in-a-react-app/
  */
-const encode = (data: {
-  [k: string]: Parameters<typeof encodeURIComponent>[0];
-}) => {
+const encode = (data: Dict<Parameters<typeof encodeURIComponent>[0]>) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
