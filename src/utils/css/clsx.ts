9dcf7d03bf6e5ff03b6cfdcb6d5d53f9fe1a04da
yuta <gssbtsm999@gmail.com>
2024-09-26 20:14:03 +0900

    refactor(clsx): 引数の型を絞る, export

--- src/utils/css/clsx.ts
+++ src/utils/css/clsx.ts
@@ -1,6 +1,6 @@
 import { twMerge } from 'tailwind-merge';
 
-type ClassInput = string | number | boolean | null | undefined;
+export type ClassInput = string | boolean | undefined;
 
 /**
  * クラス名を結合する (`twMerge`も織込み済み)
