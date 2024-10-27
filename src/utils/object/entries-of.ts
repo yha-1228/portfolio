df020327d780baa0a4bb0af37b64e4d8f0670972
yuta <gssbtsm999@gmail.com>
2024-10-27 10:07:26 +0900

    refactor(entriesOf): keyをstring onlyにする

--- src/utils/object/entries-of.ts
+++ src/utils/object/entries-of.ts
@@ -1,13 +1,15 @@
 /**
- * 型推論される`Object.entries`
+ * 型推論される`Object.entries`の代わり (keyはstring only)
  */
 export function entriesOf<T extends object>(o: T) {
   const result: [keyof T, T[Extract<keyof T, string>]][] = [];
 
   for (const key in o) {
     if (Object.prototype.hasOwnProperty.call(o, key)) {
-      const value = o[key];
-      result.push([key, value]);
+      if (typeof key === "string") {
+        const value = o[key];
+        result.push([key, value]);
+      }
     }
   }
 
